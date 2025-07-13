dfb006fbaaff86169134577059b7f2207fcb39b4
gustavomartinezx <gustavopalmr@gmail.com>
2025-07-13 20:57:22 -0300

    ♻️chore: tirando os status mockados do extensionista do base.html

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,7 +1,15 @@
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from apps.extensionistas.models import Extensionista
 
 # Create your views here.
 @login_required
 def home(request):
-    return render(request, "base.html")
+    ext_total = Extensionista.objects.count()
+    ext_ativos = Extensionista.objects.filter(status=True).count()
+    context = {
+        'ext_total': ext_total,
+        'ext_ativos': ext_ativos,
+    }
+    return render(request, "base.html", context)
