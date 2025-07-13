f74bab82c5a1ee2b2e98eb40e3f2067f5d200f00
gustavomartinezx <gustavopalmr@gmail.com>
2025-07-13 20:28:49 -0300

    ✨feat: adicionando opção de delete + separação por semestre

--- apps/extensionistas/urls.py
+++ apps/extensionistas/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('home/', views.extensionista_list, name='extensionista_list'),
     path('novo/', views.extensionista_create, name='extensionista_create'),
     path('editar/<int:pk>/', views.extensionista_update, name='extensionista_update'),
+    path('deletar/<int:pk>/', views.extensionista_delete, name='extensionista_delete'),
 ]
--- apps/extensionistas/views.py
+++ apps/extensionistas/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Extensionista
 from .forms import ExtensionistaForm
 
+
+def extensionista_delete(request, pk):
+    extensionista = get_object_or_404(Extensionista, pk=pk)
+    if request.method == "POST":
+        extensionista.delete()
+        return redirect("extensionistas:extensionista_list")
+    return render(request, "extensionista_confirm_delete.html", {"extensionista": extensionista})
+
 def extensionista_list(request):
     extensionistas = Extensionista.objects.all()
-    return render(request, "extensionistas.html", {"extensionistas": extensionistas})
+    semestres = set()
+    for ext in extensionistas:
+        year = ext.created_at.year
+        month = ext.created_at.month
+        semestre = f"{year}.1" if month <= 6 else f"{year}.2"
+        semestres.add(semestre)
+    semestres = sorted(semestres, reverse=True)
+    return render(request, "extensionistas.html", {"extensionistas": extensionistas, "semestres": semestres})
 
 def extensionista_create(request):
     if request.method == "POST":
@@ -25,4 +40,4 @@ def extensionista_update(request, pk):
             return redirect("extensionistas:extensionista_list")
     else:
         form = ExtensionistaForm(instance=extensionista)
-    return render(request, "extensionista_form.html", {"form": form})
+    return render(request, "extensionista_update.html", {"form": form})
