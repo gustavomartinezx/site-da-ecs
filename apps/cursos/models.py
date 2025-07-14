02f89c8b09511fd4930c32b63bad656294529c69
gustavomartinezx <gustavopalmr@gmail.com>
2025-07-14 17:30:35 -0300

    ♻️chore: Mudança da estrutura do DB em curso

--- apps/cursos/models.py
+++ apps/cursos/models.py
@@ -8,6 +8,8 @@ class Curso(models.Model):
     ]
     nome = models.CharField(max_length=100)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default="ativo")
+    instrutores = models.ManyToManyField('extensionistas.Extensionista', related_name='cursos', blank=True)
+    alunos = models.ManyToManyField('aluno.Aluno', related_name='cursos', blank=True)
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(null=True, blank=True)
 
